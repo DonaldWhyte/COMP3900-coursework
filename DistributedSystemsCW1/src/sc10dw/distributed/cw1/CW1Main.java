2539168244705db5f9cf8d5953ee2d01f0e77299
Donald Whyte <sc10dw@cslin040.csunix.comp.leeds.ac.uk>
2013-10-13 18:16:54 +0100

    Now display information about the database type and version being used, as well as JDBC version and drivers

--- DistributedSystemsCW1/src/sc10dw/distributed/cw1/CW1Main.java
+++ DistributedSystemsCW1/src/sc10dw/distributed/cw1/CW1Main.java
@@ -3,6 +3,7 @@ package sc10dw.distributed.cw1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.util.List;
 
 public class CW1Main {
@@ -12,15 +13,23 @@ public class CW1Main {
 		Connection dbConnection = null;
 		
 		try {
+			// Establish connection and display information about the database
 			dbConnection = ConnectionManager.getConnection();
+			DatabaseMetaData dbMetadata = dbConnection.getMetaData();
+			System.out.println("Database Product Name: " + dbMetadata.getDatabaseProductName());
+			System.out.println("Database Product Version: " + dbMetadata.getDatabaseProductVersion());
+			System.out.println("Driver Name: " + dbMetadata.getDriverName());
+			System.out.println("Driver Version: " + dbMetadata.getDriverVersion());
+			System.out.println("JDBC Version" + dbMetadata.getJDBCMajorVersion() + "." + dbMetadata.getJDBCMinorVersion() );
+			System.out.println("");
+			// Display database schema
+					
 			EmployeeRetriever retriever = new EmployeeRetriever(dbConnection);
-			List<Employee> employees = null;
-		
-			// Ask user for surname of employee to such for
+			List<Employee> employees = null;			
+			// Ask user for surname of employee to search for
 			System.out.print("Enter surname of employee to retrieve (or \"--ALL\" for all employees): ");
 			BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 			String surname = inputReader.readLine();
-			
 			// Based on input, retrieve all employees or look for
 			// employees with a specific surname
 			if (surname.equals("--ALL")) {
@@ -28,7 +37,6 @@ public class CW1Main {
 			} else {
 				employees = retriever.getEmployees(surname);
 			}
-			
 			// Output result of query
 			System.out.println("Found Employees\n-----------------------------");
 			for (Employee employee : employees) {
